a09b3e16d93b57df07d5aa75ace5a97c18074a5c
sjoseph-22 <officialsjo22@gmail.com>
2026-02-08 18:22:33 +0100

    Upload

---
+++ Controllers/ExpensesController.cs
@@ -0,0 +1,43 @@
+﻿using FinanceApp.Data;
+using FinanceApp.Data.Service;
+using FinanceApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceApp.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        private readonly IExpensesService _expensesService;
+        public ExpensesController(IExpensesService expensesService)
+        {
+            _expensesService = expensesService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var expenses = await _expensesService.GetAll();
+            return View(expenses);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Expense expense)
+        {
+            if(ModelState.IsValid)
+            {
+                await _expensesService.Add(expense);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(expense);
+        }
+        public IActionResult GetChart()
+        {
+            var data = _expensesService.GetChartData();
+            return Json(data);
+        }
+    }
+}
